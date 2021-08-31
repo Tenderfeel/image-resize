1f226093093435a080aed33beebc0798f959e37b
Tenderfeel <tenderfeel@gmail.com>
2021-08-31 12:44:38 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Image Resize Task
+
+Sharpによって画像をリサイズするGulpタスクのサンプル。  
+
+
+## Command
+Node.js v14.17.5(lts/fermium) 以上で動作、v16.7.0 推奨
+
+```bash
+npm install
+npm start
+
+# or 
+
+yarn install
+yarn start
+```
+
+## Photo Copyright
+<a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+- <a href="https://unsplash.com/@ludemeula?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ludemeula Fernandes</a>
+- <a href="https://unsplash.com/@madhatterzone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Manja Vitolic</a>
+- <a href="https://unsplash.com/@snowboardinec?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Max Baskakov</a>
+- <a href="https://unsplash.com/@e_d_g_a_r?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Edgar</a> 
+- <a href="https://unsplash.com/@cedric_photography?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Cédric VT</a>
+- <a href="https://unsplash.com/@theluckyneko?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">The Lucky Neko</a>
+- <a href="https://unsplash.com/@whatisdelirium?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nadi Whatisdelirium</a>
+  
---
+++ gulpfile.esm.js
@@ -0,0 +1,76 @@
+import sharp from 'sharp';
+import { readdir, readFile, writeFile } from 'fs/promises'
+import del from 'del';
+import chalk from 'chalk'
+import ora from 'ora'
+
+// 処理前のディレクトリ
+const IMAGE_DIRECTORY_PATH = './images/'
+
+// 処理後のディレクトリ
+const DIST_DIRECTORY_PATH = './dist/'
+
+// リサイズ設定
+const RESIZE_OPTION = {
+  fit: 'cover',
+  width: 600,
+  height: 600,
+  position: sharp.strategy.attention,
+}
+
+// 処理後の画像保存形式
+const DIST_FORMAT = 'jpg'
+
+// 処理後にimagesの画像を削除する
+const isDeleteSrcImg = false
+
+
+const spinner = ora(`Start resize images`).start(
+  `Read ${chalk.bold(IMAGE_DIRECTORY_PATH)} directory`
+)
+
+async function defaultTask(cb) {
+  const files = await readdir(IMAGE_DIRECTORY_PATH, { withFileTypes: true })
+  const filterFiles = files.filter((file) => file.isFile() && !file.name.includes('.DS_Store'))
+
+  spinner
+    .succeed(`Read ${chalk.bold(files.length)} image files.`)
+    .start(`Start image resize process.`)
+
+  for (let i = 0; i < filterFiles.length; i++) {
+    const file = filterFiles[i]
+    spinner.start(`${chalk.bold(`${file.name}`)} prosess...`)
+    await imageCompress(file)
+  }
+
+  spinner.succeed('image file export!')
+  
+  cb();
+}
+
+function imageCompress(file) {
+  return new Promise((resolve, reject) => {
+    const [ fileName, ex ] = file.name.split('.')
+    sharp(`${IMAGE_DIRECTORY_PATH}${file.name}`)
+    .resize(RESIZE_OPTION)
+    .toFormat(DIST_FORMAT)
+    .toFile(`${DIST_DIRECTORY_PATH}${fileName}.${DIST_FORMAT}`, (err, info) => {
+      if(err) {
+        spinner.fail(`${chalk.bold(`${file.name}`)} ${err}`)
+        return reject(err)
+      } 
+
+      if (!isDeleteSrcImg) {
+        spinner.succeed(`${chalk.bold(`${file.name}`)} resized.`)
+        return resolve(info)
+      }
+
+      del([`${IMAGE_DIRECTORY_PATH}${file.name}`]).then(() => {
+        spinner.succeed(`${chalk.bold(`${file.name}`)} resized.`)
+        resolve(info)
+      })
+    })
+  })
+}
+
+export default defaultTask
